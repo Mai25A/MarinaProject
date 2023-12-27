3719d456e3d96465689cbb69106d26d05dda1077
Mai <mai.ho25@student.passerellesnumeriques.org>
2023-12-27 07:40:30 +0700

    Code interface for table page

--- views/css/table.css.php
+++ views/css/table.css.php
@@ -1,9 +1,47 @@
 <style>
-        .custom-button {
+
+       .custom-button {
             height: 35px;
             width: 150px;
-            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
             border-radius: 10px;
             border:1px solid #26577C;
+            background-color: white;
+        }
+        .btn_type:focus{
+            background-color: #26577C;
+            color: white;
+            text-align: center;
+            justify-content: center;
+        }
+        .button_con{
+            display: flex;
+        }
+        .form-select{
+            float: right;
+        }
+        .list-item{
+            border-radius: 10px;
+            height: 370px;
+            box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.2);
+        }
+        .image-item{    
+            margin: 10px;
+            margin-top: 20px;
+            width: 22rem;
+            height: 60%;
+        }
+        .image-item img {
+            border-radius: 10px;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .card-price{
+            font-size: large;
+            font-weight: bold;
+        }
+        .card-price .text-end{
+            margin-top: -15px;
         }
  </style>
