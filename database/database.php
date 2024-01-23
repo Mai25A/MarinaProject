4755ee848572de76656e7b5baf422cd1a5950cfa
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-01-23 12:52:36 +0700

    Mai fix database

--- database/database.php
+++ database/database.php
@@ -7,10 +7,10 @@ $password = "";
 
 
 try {
-    $connection = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
-  ?>
+  $connection = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
